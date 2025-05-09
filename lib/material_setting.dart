9c0bd68294513a48bdd41ab28360a1c0ee3f576a
khaledelnaghy <khaledelnaghy777@gmail.com>
2025-05-09 22:51:41 +0300

    splash screen and screen utils is done

--- lib/material_setting.dart
+++ lib/material_setting.dart
@@ -2,6 +2,7 @@ import 'package:advanced_ecommerce/core/app/connectivity_controller.dart';
 import 'package:advanced_ecommerce/core/app/env.variables.dart';
 import 'package:advanced_ecommerce/core/common/screens/no_network_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AdvancedEcommerce extends StatelessWidget {
   const AdvancedEcommerce({super.key});
@@ -12,17 +13,21 @@ class AdvancedEcommerce extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (_, value, __) {
         if (value) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
-            builder: (context, widget) {
-              return Scaffold(
-                body: Builder(builder: (context) {
-                  ConnectivityController.instance.init();
-                  return widget!;
-                }),
-              );
-            },
-            home: const Scaffold(),
+          return ScreenUtilInit(
+            designSize: const Size(375, 812),
+            minTextAdapt: true,
+            child: MaterialApp(
+              debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
+              builder: (context, widget) {
+                return Scaffold(
+                  body: Builder(builder: (context) {
+                    ConnectivityController.instance.init();
+                    return widget!;
+                  }),
+                );
+              },
+              home: const Scaffold(),
+            ),
           );
         } else {
           return MaterialApp(
