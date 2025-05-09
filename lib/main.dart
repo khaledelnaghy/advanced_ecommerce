4152b952a9acf5bbc3339e154f39a56f253c0a37
khaledelnaghy <khaledelnaghy777@gmail.com>
2025-05-09 14:59:02 +0300

    state app

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:advanced_ecommerce/material_setting.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AdvancedEcommerce());
+}
+
+ 
---
+++ lib/material_setting.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AdvancedEcommerce extends StatelessWidget {
+  const AdvancedEcommerce({super.key});
+
+   
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    
+      home: const ,
+    );
+  }
+}
