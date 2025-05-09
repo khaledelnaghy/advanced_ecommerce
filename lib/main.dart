d76452e9d05f702e7f91d1e495bd9f8bbd1e5064
khaledelnaghy <khaledelnaghy777@gmail.com>
2025-05-09 18:52:50 +0300

    env created done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:advanced_ecommerce/core/app/env.variables.dart';
 import 'package:advanced_ecommerce/firebase_options.dart';
 import 'package:advanced_ecommerce/material_setting.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EnvVariables.instance.init(envType: EnvTypeEnum.production);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
   await Firebase.initializeApp();
-  runApp(const AdvancedEcommerce());
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  ).then((_) {
+    runApp(const AdvancedEcommerce());
+  });
 }
