34f04d73b5070b65839209895889df75f460ae18
khaledelnaghy <khaledelnaghy777@gmail.com>
2025-05-09 19:49:22 +0300

    connectivity plus done

--- lib/material_setting.dart
+++ lib/material_setting.dart
@@ -1,4 +1,6 @@
+import 'package:advanced_ecommerce/core/app/connectivity_controller.dart';
 import 'package:advanced_ecommerce/core/app/env.variables.dart';
+import 'package:advanced_ecommerce/core/common/screens/no_network_screen.dart';
 import 'package:flutter/material.dart';
 
 class AdvancedEcommerce extends StatelessWidget {
@@ -6,9 +8,29 @@ class AdvancedEcommerce extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   MaterialApp(
-      debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
-      home: const Scaffold(),
+    return ValueListenableBuilder(
+      valueListenable: ConnectivityController.instance.isConnected,
+      builder: (_, value, __) {
+        if (value) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
+            builder: (context, widget) {
+              return Scaffold(
+                body: Builder(builder: (context) {
+                  ConnectivityController.instance.init();
+                  return widget!;
+                }),
+              );
+            },
+            home: const Scaffold(),
+          );
+        } else {
+          return MaterialApp(
+            debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
+            home: const NoNetworkScreen(),
+          );
+        }
+      },
     );
   }
 }
