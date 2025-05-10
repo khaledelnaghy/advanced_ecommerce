4932a08057ba1d7f5f38b7cac963b672d8e0d228
khaledelnaghy <khaledelnaghy777@gmail.com>
2025-05-10 19:53:50 +0300

    login ui done

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,24 +1,20 @@
 import 'package:advanced_ecommerce/core/common/screens/under_builder_screen.dart';
 import 'package:advanced_ecommerce/core/routes/base_routes.dart';
-import 'package:advanced_ecommerce/test_one_screen.dart';
-import 'package:advanced_ecommerce/test_two_screen.dart';
-import 'package:flutter/material.dart';
+import 'package:advanced_ecommerce/features/auth/presentation/screens/login_screen.dart';
+  import 'package:flutter/material.dart';
 
 class AppRoutes {
-  static const String testOne = 'test_one';
+  static const String login = 'login';
   static const String testTwo = 'test_two';
 
   static Route<void> onGenerateRoute(RouteSettings settings) {
     final args = settings.arguments;
     switch (settings.name) {
-      case testOne:
+      case login:
         return BaseRoute(
-          page: const TestOneScreen(),
+          page: const LoginScreen(),
         );
-      case testTwo:
-        return BaseRoute(
-          page:const TestTwoScreen(),
-        );
+       
       default:
         return BaseRoute(
           page:const PageUnderBuildScreen(),
---
+++ lib/features/auth/presentation/widgets/auth_title_info.dart
@@ -0,0 +1,44 @@
+import 'package:advanced_ecommerce/core/common/animations/animate_do.dart';
+import 'package:advanced_ecommerce/core/common/widgets/text_app.dart';
+import 'package:advanced_ecommerce/core/extensions/context_extensions.dart';
+import 'package:advanced_ecommerce/core/style/fonts/font_weight_hepler.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AuthTitleInfo extends StatelessWidget {
+  const AuthTitleInfo(
+      {required this.title, required this.description, super.key});
+  final String title;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomFadeInDown(
+      duration: 400,
+      child: Column(
+        children: [
+          TextApp(
+            text: title,
+            theme: context.textStyle.copyWith(
+              fontSize: 24.sp,
+              fontWeight: FontWeightHepler.bold,
+              color: context.color.textColor,
+            ),
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          TextApp(
+            text: description,
+            theme: context.textStyle.copyWith(
+              fontSize: 16.sp,
+              fontWeight: FontWeightHepler.medium,
+              color: context.color.textColor,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
